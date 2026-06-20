b667ef20091873193e01a3583e604a179c01de98
elabdioui <elabdiouihaitam@gmail.com>
2026-06-20 14:36:04 +0100

    Remove legacy gold strategies

--- detector/strategy/__init__.py
+++ detector/strategy/__init__.py
@@ -1,17 +1,7 @@
-from .tier_s import scan_golden_setup
-from .tier_a import scan_ob_retest, scan_asia_fade
-from .tier_b import scan_breaker_fib, scan_bos_fvg
-from .tier_swing import scan_break_retest
-from .orb import scan_orb_ny
 from .killzone import get_active_killzone, is_in_killzone, minutes_to_next_killzone
 from .killzone import KILLZONE_PRIORITY
 
 __all__ = [
-    "scan_golden_setup",
-    "scan_ob_retest", "scan_asia_fade",
-    "scan_breaker_fib", "scan_bos_fvg",
-    "scan_break_retest",
-    "scan_orb_ny",
     "get_active_killzone", "is_in_killzone", "minutes_to_next_killzone",
     "KILLZONE_PRIORITY",
 ]
